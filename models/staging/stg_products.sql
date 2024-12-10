5b0ab8805d37497cd24af41131c8d58664aef67d
Mostafa Jarmouni <mostafa.jarmouni@cognizant.com>
2024-12-10 11:35:40 +0000

    Fixing stg_products.sql

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,8 +1,6 @@
 {{ config(  materialized = 'table', 
             schema=env_var('DBT_STAGESCHEMA','STAGING_DEV'),
-            transient= false, 
-            pre_hook = "use warehouse loading_wh;", 
-            post_hook = "create or replace table stg_products_save clone stg_products;") }}
+            transient= false) }}
 
 select *
 from {{ source('qwt_raw', 'products')}}
