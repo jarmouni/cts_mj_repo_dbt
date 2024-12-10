f1bacadf92279dbf11b943257bebe44693175205
Mostafa Jarmouni <mostafa.jarmouni@cognizant.com>
2024-12-10 12:57:06 +0000

    Modifying stg_products.sql model

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,6 +1,6 @@
 {{ config(  materialized = 'table', 
-            schema=env_var('DBT_STAGESCHEMA','STAGING_DEV'),
-            transient= false) }}
+            schema = env_var('DBT_STAGESCHEMA','STAGING_DEV'),
+            transient = false) }}
 
 select *
 from {{ source('qwt_raw', 'products')}}
